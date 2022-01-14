1ce4e530b9608ebc7a9a4938fcfc3b28afdd1c10
dante <maxgamerdu@gmail.com>
2022-01-14 10:32:20 +0300

    adding components that i will be referencing such as the user, org and dash pages individually

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DashComponent } from './dash/dash.component';
+import { UserComponent } from './user/user.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DashComponent,
+    UserComponent
   ],
   imports: [
     BrowserModule,
