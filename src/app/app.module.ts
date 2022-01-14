e9ff56e56861d831fda8ce8f9d183e3fc947298d
dante <maxgamerdu@gmail.com>
2022-01-14 10:35:35 +0300

    imported a (waitforasync) sys

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { DashComponent } from './dash/dash.component';
 import { UserComponent } from './user/user.component';
+import { OrganisationComponent } from './organisation/organisation.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     DashComponent,
-    UserComponent
+    UserComponent,
+    OrganisationComponent
   ],
   imports: [
     BrowserModule,
