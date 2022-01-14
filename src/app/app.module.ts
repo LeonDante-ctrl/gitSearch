e235298036951dc4c72d0f8289bbb53b4463c66c
dante <maxgamerdu@gmail.com>
2022-01-14 10:50:29 +0300

    added all my imports to my app.module such as my set components as well as pipes and also keyed in my providers for this projects functionality

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,24 +1,31 @@
-import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
 
+import { GithubService } from './github-service';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { DashComponent } from './dash/dash.component';
 import { UserComponent } from './user/user.component';
+import { DashComponent } from './dash/dash.component';
 import { OrganisationComponent } from './organisation/organisation.component';
+import { RepositoryComponent } from './repository/repository.component';
+import { TruncatePipe } from './truncate.pipe';
 
 @NgModule({
   declarations: [
     AppComponent,
-    DashComponent,
     UserComponent,
-    OrganisationComponent
+    DashComponent,
+    OrganisationComponent,
+    RepositoryComponent,
+    TruncatePipe,
   ],
   imports: [
     BrowserModule,
+    HttpClientModule,
     AppRoutingModule
   ],
-  providers: [],
+  providers: [GithubService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
